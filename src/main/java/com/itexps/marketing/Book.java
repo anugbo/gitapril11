55c9110217752cae6e12591c29e5c93a451985bc
anugbo <57274149+anugbo@users.noreply.github.com>
2020-04-11 14:09:57 -0500

    initial copy

---
+++ src/main/java/com/itexps/marketing/Book.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itexps.marketing;
+
+/**
+ *
+ * @author okoob
+ */
+public class Book {
+    String name;
+    String title;
+    int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+  
+    
+    
+}
---
+++ src/test/java/test/com/itexps/marketing/BookTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com.itexps.marketing;
+
+import com.itexps.marketing.Book;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author okoob
+ */
+public class BookTest {
+    Book book;
+    
+    public BookTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        book = new Book();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testBook() {
+        book.setName("Literature");
+        book.setTitle("Animal Farm");
+        book.setPrice(111);
+        
+        assertEquals ("Literature", book.getName());
+        assertEquals ("Animal Farm", book.getTitle());
+        assertEquals (111, book.getPrice());
+        
+    }
+}
